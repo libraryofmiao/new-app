953892a900dfb2d70af687b3825c3abe6c0614c5
libraryofmiao <libraryofmiao@gmail.com>
2026-09-17 09:45:56 +0530

    Align Android app with stable Kotlin and Codespaces SDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,18 +1,16 @@
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-
 plugins {
     id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
     namespace = "in.miaolibrary.patron"
-    // SDK 34 is widely available in Codespaces and local Android toolchains.
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "in.miaolibrary.patron"
-        minSdk = 26
-        targetSdk = 34
+        minSdk = 23
+        targetSdk = 35
         versionCode = 1
         versionName = "0.1.0"
     }
@@ -31,10 +29,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
-}
 
-kotlin {
-    compilerOptions {
-        jvmTarget = JvmTarget.JVM_17
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
