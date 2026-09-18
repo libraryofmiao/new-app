0deb457b4a775dccaca5af4ad2b46c308920a95e
libraryofmiao <libraryofmiao@gmail.com>
2026-09-18 20:18:29 +0530

    Use bundled advanced library icon pack and app icon

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,8 +29,37 @@ kotlin {
     }
 }
 
+val generatedLibraryAssets = layout.buildDirectory.dir("generated/libraryIcons/assets")
+val generatedLibraryRes = layout.buildDirectory.dir("generated/libraryIcons/res")
+
+val prepareLibraryIcons by tasks.registering {
+    inputs.file(rootProject.file("advanced_library_icons.zip"))
+    outputs.dir(generatedLibraryAssets)
+    outputs.dir(generatedLibraryRes)
+    doLast {
+        copy {
+            from(zipTree(rootProject.file("advanced_library_icons.zip")))
+            include("advanced_library_icons/*.svg")
+            into(generatedLibraryAssets.get().asFile)
+        }
+        copy {
+            from(zipTree(rootProject.file("advanced_library_icons.zip"))) {
+                include("advanced_library_icons/App_icon.png")
+                eachFile { relativePath = RelativePath(true, "app_icon.png") }
+                includeEmptyDirs = false
+            }
+            into(generatedLibraryRes.get().asFile.resolve("drawable"))
+        }
+    }
+}
+
+android.sourceSets["main"].assets.srcDir(generatedLibraryAssets)
+android.sourceSets["main"].res.srcDir(generatedLibraryRes)
+tasks.named("preBuild").configure { dependsOn(prepareLibraryIcons) }
+
 dependencies {
     implementation("androidx.core:core-ktx:1.15.0")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("com.caverock:androidsvg-aar:1.4")
 }
